b96b47c36f0e88e5794d86cdcd349dfe81b136d2
sartic <alan_sudo@yeah.net>
2025-02-11 20:39:22 +0800

    added new backbone: neoformer

--- modules/backbones/__init__.py
+++ modules/backbones/__init__.py
@@ -1,13 +1,14 @@
 import torch.nn
 from modules.backbones.wavenet import WaveNet
 from modules.backbones.lynxnet import LYNXNet
+from modules.backbones.neoformer import DiffsingerNeoformer
 from utils import filter_kwargs
 
 BACKBONES = {
     'wavenet': WaveNet,
-    'lynxnet': LYNXNet
+    'lynxnet': LYNXNet,
+    'neoformer': DiffsingerNeoformer
 }
-
 
 def build_backbone(
         out_dims: int, num_feats: int,
